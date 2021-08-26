909ed647ee1752d1f65ae8a98e3d3970f1643f7c
JarrColl <jarronc@protonmail.com>
2021-08-26 14:27:47 +1000

    Fixed manifest.json matches so it runs without refresh in watch page

--- skipIntro.js
+++ skipIntro.js
@@ -1,25 +1,28 @@
+//TODO: differentiate between recap, intro. Settings to enable skipping of each.
 console.log("SkipIntro Initialised");
+
+let observer = new MutationObserver((mutations) => {
+    for(let mutation of mutations)
+    {
+        if (mutation.type === 'childList') {
+            console.log('A child node has been added or removed.');
+            for(let node of mutation.addedNodes)
+            {
+                if(node.classList.contains("skip-credits"))
+                {
+                    console.log("Skipped Intro");
+                    node.getElementsByTagName("a")[0].click();
+                }
+            }
+        }
+    }
+});
 
 //window.addEventListener('DOMContentLoaded', () => {
     const targetNode = document.getElementsByClassName("PlayerControlsNeo__layout")[0] || document;
     console.log(targetNode);
     // /PlayerControlsNeo__layout
-    let observer = new MutationObserver((mutations) => {
-        for(let mutation of mutations)
-        {
-            if (mutation.type === 'childList') {
-                console.log('A child node has been added or removed.');
-                for(let node of mutation.addedNodes)
-                {
-                    if(node.classList.contains("skip-credits"))
-                    {
-                        console.log("Skipped Intro");
-                        node.getElementsByTagName("a")[0].click();
-                    }
-                }
-            }
-        }
-    });
+
     
     observer.observe(targetNode, {childList: true, subtree: true});
 //}, false);
